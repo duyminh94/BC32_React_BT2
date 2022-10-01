a961ba8fb5e2821c2d9577f874c3890403e4f203
Le Nguyen Duy Minh <108447811+duyminh94@users.noreply.github.com>
2022-10-01 21:30:34 +0700

    uploadsouce

--- src/1_Companent/GlassesList.jsx
+++ src/1_Companent/GlassesList.jsx
@@ -6,7 +6,7 @@ export const GlassesList = ({ products, onSelect }) => {
                 <div className='row mt-2'>
                     {products.map((product => {
                         return (
-                            <div key={product.id} className="col-lg-2 p-1">
+                            <div key={product.id} className="col-sm-2 p-1">
                                 <button onClick={() => onSelect(product)}>
                                     <img className='img-fluid' src={product.url} alt="#" />
                                 </button>
